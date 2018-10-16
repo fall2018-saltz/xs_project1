be4ec3afa4baa41950e1b7e690154b6e981e8fbd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 10:12:53 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -8,3 +8,5 @@ head(mergedDF)
 # Drop the Row.names column
 mergedDF2 <- mergedDF[ -c(1) ]
 head(mergedDF2)
+
+4#) Add the area of each state, and the center of each state, to the merged dataframe, using the ‘state.area’, ‘state.center’ and ‘state.name’ vectors
