67d2044dc742aec4787c7c27c17023eef17cca35
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 15:27:56 -0400

    Saving StepCD.R

--- StepCD.R
+++ StepCD.R
@@ -9,16 +9,14 @@ mergedDF2$stateNames <- tolower(mergedDF2$state.name)
 us=map_data("state")
 
 map.simple <- ggplot(mergedDF2, aes(map_id = stateNames))
-
 map.simple <- map.simple + geom_map(map = us, aes(fill=mergedDF2$Murder))
- 
 map.simple <- map.simple + expand_limits(x=us$long,y=us$lat) + coord_map()
-
 map.simple
 #2) Show the population as a circle per state (the larger the population, 
 # the larger the circle), using the location defined by the center of each state
 map.simple2 <- map.simple + geom_point(data=mergedDF2,aes(x=mergedDF2$x,y=mergedDF2$y,size=mergedDF2$population))
 map.simple2
 
-#3) Repeat step C, but only show the states in the north east <- map_data("state", region="louisiana")
+#3) Repeat step C, but only show the states in the north east
+ne<- map_data("state", region="north east")
 
