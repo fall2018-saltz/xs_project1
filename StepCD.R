e8266a4df753df55d3d01d1d8b395bcf42cf63ec
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 17:06:12 -0400

    Saving StepCD.R

--- StepCD.R
+++ StepCD.R
@@ -21,7 +21,8 @@ map.simple2
 
 #3) Repeat step C, but only show the states in the north east
 
-# require('RDSTK')
+ny <- geocode("New York")
+#require('RDSTK')
 # test <- street2coordinates("2543 Graystone Place, Simi Valley, CA 93065")
 # test
 
