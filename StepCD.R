a5ad69e074611228acbe4b4eec06a73d68b48d10
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 16:10:17 -0400

    Saving StepCD.R

--- StepCD.R
+++ StepCD.R
@@ -20,6 +20,7 @@ map.simple2 <- map.simple + geom_point(data=mergedDF2,aes(x=mergedDF2$x,y=merged
 map.simple2
 
 #3) Repeat step C, but only show the states in the north east
+curl "http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=790+7th+Avenue,+Mountain+View,+NY"
 ny <- geocode("new york")
 ny
 ne=map_data("state", xlim=c(ny$lon-10,ny$lon+10), ylim=c(ny$lat-10,ny$lat+10))
