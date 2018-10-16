dd06101e930c6601c2b2a3337d8f3ba2c091c120
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 10:41:08 -0400

    Saving StepB.R

--- StepB.R
+++ StepB.R
@@ -6,6 +6,6 @@ library(ggplot2)
 library(ggmap)
 us=map_data("state")
 map.simple <- ggplot(mergedDF2,aes(map_id=state))
-map.simple <- map.simple + geom_map(map = us, fill = region, color = "black")
+map.simple <- map.simple + geom_map(map = us, fill = "light bule", color = "black")
 #map.simple <- map.simple + geom_polygon(aes(fill = region, group = group), color = "white")
 map.simple
