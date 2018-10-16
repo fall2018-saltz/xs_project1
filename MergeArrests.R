62e9f7212930f78fc7a1f30cefcc765bcfe8ad40
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 10:23:45 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -6,8 +6,8 @@ row.names(popState) <- popState$stateNames
 mergedDF <- merge(popState, arrests, by=0, all=TRUE)
 head(mergedDF)
 # Drop the Row.names column
-mergedDF2 <- mergedDF[ -c(1) ]
-head(mergedDF2)
+mergedDF<- mergedDF[ -c(1) ]
+head(mergedDF)
 
 #4) Add the area of each state, and the center of each state, 
 #to the merged dataframe, using the ‘state.area’, ‘state.center’ and ‘state.name’ vectors
@@ -15,5 +15,7 @@ library(ggmap)
 library(ggplot2)
 stateDF <- data.frame(state.name,state.area,state.center)
 head(stateDF)
-mergedDF3 <- merge(mergedDF2, stateDF, by=0, all=TRUE)
-head(mergedDF3)
+mergedDF2 <- merge(mergedDF, stateDF, by=0, all=TRUE)
+head(mergedDF2)
+mergedDF<- mergedDF[ -c(1) ]
+head(mergedDF)
