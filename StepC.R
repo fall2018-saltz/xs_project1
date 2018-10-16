737d089828f8ecd6a076c81968000f955b58bb59
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 15:17:39 -0400

    Saving StepC.R

--- StepC.R
+++ StepC.R
@@ -17,4 +17,4 @@ map.simple <- map.simple + expand_limits(x=us$long,y=us$lat) + coord_map()
 map.simple
 #2) Show the population as a circle per state (the larger the population, 
 # the larger the circle), using the location defined by the center of each state
-map.simple2 <- map.simple + geom_point(mergedDF2,aes(x=mergedDF2$x,y=mergedDF2$y,size=mergedDF2$population))
+map.simple2 <- map.simple + geom_point(data=mergedDF2,aes(x=mergedDF2$x,y=mergedDF2$y,size=mergedDF2$population))
