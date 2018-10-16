94a779a65fe378914cbc7e9627d2918a5dece686
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 10:13:26 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -9,4 +9,6 @@ head(mergedDF)
 mergedDF2 <- mergedDF[ -c(1) ]
 head(mergedDF2)
 
-4#) Add the area of each state, and the center of each state, to the merged dataframe, using the ‘state.area’, ‘state.center’ and ‘state.name’ vectors
+#4) Add the area of each state, and the center of each state, 
+#to the merged dataframe, using the ‘state.area’, ‘state.center’ and ‘state.name’ vectors
+stateName <- state.stateName
