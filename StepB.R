9f412d1dc50ffdf28d1c9841588638c77a335929
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 11:02:01 -0400

    Saving StepB.R

--- StepB.R
+++ StepB.R
@@ -9,7 +9,7 @@ library(maps)
 
 us=map_data("state")
 
-map.simple <- ggplot(data= mergedDF2, aes(map_id = stateName,fill=factor(overall.quantiles)))
+map.simple <- ggplot(data= mergedDF2, aes(map_id = state.name,fill=factor(overall.quantiles)))
 
 map.simple <- map.simple + geom_map(map = us, fill = "light bule", color = "black")
 map.simple
