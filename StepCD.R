fef41c0c715cf10ac971adec67203250317d1f77
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 15:28:53 -0400

    Saving StepCD.R

--- StepCD.R
+++ StepCD.R
@@ -24,3 +24,6 @@ map.simple3 <- ggplot(mergedDF2, aes(map_id = stateNames))
 map.simple3 <- map.simple3 + geom_map(map = us, aes(fill=mergedDF2$Murder))
 map.simple3 <- map.simple3 + expand_limits(x=us$long,y=us$lat) + coord_map()
 map.simple3
+
+map.simple4 <- map.simple3 + geom_point(data=mergedDF2,aes(x=mergedDF2$x,y=mergedDF2$y,size=mergedDF2$population))
+map.simple4
