a1c7d1757251f05106a9b2f0ee32c323671fc025
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:57:15 -0400

    Saving StepCD.R

--- StepCD.R
+++ StepCD.R
@@ -8,7 +8,7 @@ library(maps)
 #1) Repeat step B, but color code the map based on the murder rate of each state.
 mergedDF2$stateNames <- tolower(mergedDF2$state.name)
 us=map_data("state")
-
+us
 map.simple <- ggplot(mergedDF2, aes(map_id = stateNames))
 map.simple <- map.simple + geom_map(map = us, aes(fill=mergedDF2$Murder))
 map.simple <- map.simple + expand_limits(x=us$long,y=us$lat) + coord_map()
@@ -29,7 +29,7 @@ ny
 ne=map_data("state", xlim=c(ny$lon-10,ny$lon+10), ylim=c(ny$lat-10,ny$lat+10))
 map.simple3 <- ggplot(mergedDF2, aes(map_id = stateNames))
 map.simple3 <- map.simple3 + geom_map(map = ne, aes(fill=mergedDF2$Murder))
-map.simple3 <- map.simple3 + coord_map()
+map.simple3 <- map.simple3 + expand_limits(x=,y=) + coord_map()
 map.simple3 
 
 map.simple4 <- map.simple3 + geom_point(data=mergedDF2,aes(x=mergedDF2$x,y=mergedDF2$y,size=mergedDF2$population))
